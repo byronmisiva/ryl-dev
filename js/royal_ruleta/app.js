c08d473f404a27fbdcdab46c03f459c984a43c8e
Byron <byronherrera@hotmail.com>
2015-10-23 12:02:03 -0500

    WIP

--- js/royal_ruleta/app.js
+++ js/royal_ruleta/app.js
@@ -7,16 +7,20 @@ var animGelatinasActive = 1;
 var gelatinaPremio = 1;
 var gelPremio = getRandomInt(0, 4);
 
-var pruebas  = 1;
+var pruebas = 1;
 var ganapremio = 0;
+
+//pausa en el juego
+var pausa = false;
+var tiempoPausa = 3000;
 
 var sabores = [];
 
 
-window.onload = function() {
-    var backgroundAudio=document.getElementById("bgAudio");
-    backgroundAudio.volume=0.1;
-    backgroundAudio.src="linus-and-lucy_part_2.mp3"
+window.onload = function () {
+    var backgroundAudio = document.getElementById("bgAudio");
+    backgroundAudio.volume = 0;
+    backgroundAudio.src = "linus-and-lucy_part_2.mp3"
 }
 
 //funciones scriptcam
@@ -28,9 +32,7 @@ $(document).ready(function () {
     crearBotonesInterface();
 
 
-
 });
-
 
 
 /// fin actividad gelatinas
@@ -40,27 +42,28 @@ function animGelatina(x, animationDiv) {
 };
 
 $(document).ready(function () {
-    if (pruebas== 1){
+    if (pruebas == 1) {
         var anim = "crossscreen";
         animGelatinas(anim, "#animationGelatina1");
     }
 
     $('.js--triggerAnimation1').click(function (e) {
+        if (!pausa) {
+            $('#animationSandbox').removeClass();
+            e.preventDefault();
+            animGelatinasActive = 0;
 
-        $('#animationSandbox').removeClass();
-        e.preventDefault();
-        animGelatinasActive = 0;
-
-        $('.js--triggerAnimation1').removeClass('boton-juego').addClass('boton-juego-click');
-        setTimeout(function(){
-            $('.js--triggerAnimation1').removeClass('boton-juego-click').addClass('boton-juego')
-        } , 800)
+            $('.js--triggerAnimation1').removeClass('boton-juego').addClass('boton-juego-click');
+            setTimeout(function () {
+                $('.js--triggerAnimation1').removeClass('boton-juego-click').addClass('boton-juego')
+            }, 800)
+        }
     });
 
 
 });
 
-function muestraBotonOriginal () {
+function muestraBotonOriginal() {
 
 }
 function animGelatinas() {
@@ -71,47 +74,55 @@ function animGelatinas() {
 
 
 function lanzaGelatinas() {
-    if (animGelatinasActive == 1) {
-        var gelatinas = ["snoopy-juego\/cereza.png", "snoopy-juego\/frambuesa.png", "snoopy-juego\/limon.png", "snoopy-juego\/uva.png", "snoopy-juego\/naranja.png"];
-        var divSeleccion = getRandomInt(0, 4);
-        var nuevaGelatina = '<span id="animationGelatina' + numeroGelatina + '"><img ' +
-            'src="imagenes\/royal_ruleta\/' + gelatinas[divSeleccion] + '"' +
-            'class="img-responsive"/></span>';
-        $(".contenedorGelatina").append(nuevaGelatina);
-        var anim = "crossscreen";
-        animGelatina(anim, "#animationGelatina" + numeroGelatina);
-        numeroGelatina = numeroGelatina + 1;
-    } else {
-        if (ultimo) {
+    if (!pausa) {
+        if (animGelatinasActive == 1) {
             var gelatinas = ["snoopy-juego\/cereza.png", "snoopy-juego\/frambuesa.png", "snoopy-juego\/limon.png", "snoopy-juego\/uva.png", "snoopy-juego\/naranja.png"];
-            if (ganapremio == 1) {
-                var divSeleccion = gelPremio;
-            } else {
-                do {
-                    var divSeleccion = getRandomInt(0, 4);
-                }
-                while (isInArray(divSeleccion, sabores));
-                // ponemos los sabores que salen
-                sabores.push(divSeleccion);
-            }
-
+            var divSeleccion = getRandomInt(0, 4);
             var nuevaGelatina = '<span id="animationGelatina' + numeroGelatina + '"><img ' +
                 'src="imagenes\/royal_ruleta\/' + gelatinas[divSeleccion] + '"' +
                 'class="img-responsive"/></span>';
             $(".contenedorGelatina").append(nuevaGelatina);
-            var anim = "crossscreen" + gelatinaPremio;
-            gelatinaPremio++;
+            var anim = "crossscreen";
             animGelatina(anim, "#animationGelatina" + numeroGelatina);
             numeroGelatina = numeroGelatina + 1;
-            //ultimo = false;
-            animGelatinasActive = 1;
-            if (gelatinaPremio > 3) {
-                ultimo = false;
-                animGelatinasActive = 0;
+        } else {
+            if (ultimo) {
+                var gelatinas = ["snoopy-juego\/cereza.png", "snoopy-juego\/frambuesa.png", "snoopy-juego\/limon.png", "snoopy-juego\/uva.png", "snoopy-juego\/naranja.png"];
+                if (ganapremio == 1) {
+                    var divSeleccion = gelPremio;
+                } else {
+                    do {
+                        var divSeleccion = getRandomInt(0, 4);
+                    }
+                    while (isInArray(divSeleccion, sabores));
+                    // ponemos los sabores que salen
+                    sabores.push(divSeleccion);
+                }
 
-                setTimeout(function(){
-                    cierre ()
-                } , 2000)
+                var nuevaGelatina = '<span id="animationGelatina' + numeroGelatina + '"><img ' +
+                    'src="imagenes\/royal_ruleta\/' + gelatinas[divSeleccion] + '"' +
+                    'class="img-responsive"/></span>';
+                $(".contenedorGelatina").append(nuevaGelatina);
+                var anim = "crossscreen" + gelatinaPremio;
+                gelatinaPremio++;
+                animGelatina(anim, "#animationGelatina" + numeroGelatina);
+                numeroGelatina = numeroGelatina + 1;
+                //ultimo = false;
+                animGelatinasActive = 1;
+                if (gelatinaPremio > 3) {
+                    ultimo = false;
+                    animGelatinasActive = 0;
+
+                    setTimeout(function () {
+                        cierre()
+                    }, 5000)
+
+                }
+                // activamos la pausa
+                pausa = true;
+                setTimeout(function () {
+                    continuaJuego ()
+                }, tiempoPausa)
 
             }
         }
@@ -119,11 +130,17 @@ function lanzaGelatinas() {
 }
 // fin actividad gelatinas
 
-function cierre () {
+
+function continuaJuego () {
+    pausa = false
+
+}
+function cierre() {
+    pausa = true;
     if (ganapremio == 0) {
-        alert ("Usted a perdido");
+        alert("Usted a perdido");
     } else {
-        alert ("Usted a ganado");
+        alert("Usted a ganado");
     }
 }
 /**
