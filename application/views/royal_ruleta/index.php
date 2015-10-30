429e97baf6bf420b1ab286f2e2f0ccfeed346b99
Byron <byronherrera@hotmail.com>
2015-10-30 10:23:23 -0500

    aa

--- application/views/royal_ruleta/index.php
+++ application/views/royal_ruleta/index.php
@@ -26,7 +26,8 @@
 
     <meta property="og:title" content="Gana con Royal">
     <meta property="og:type" content="article">
-    <meta property="og:url" content="http://www.ganaconroyal.com">
+<!--    <meta property="og:url" content="http://www.ganaconroyal.com">-->
+    <meta property="og:url" content="http://royal.misiva.com.ec">
     <meta property="og:image" content="http://norfipc.com/img/logos/compartir-pagina-redes-sociales.jpeg">
     <meta property="og:site_name" content="GanaConRoyal">
 <meta property="fb:admins" content="100001543332907">
