b4afe907c459e65e6c00ffb6a467dd416bd69e17
Byron <byronherrera@hotmail.com>
2015-10-30 16:12:36 -0500

    aa

---
+++ indexproximamente.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>Royal :: Snoopy</title>
+    <meta http-equiv="content-language" content="es"/>
+    <meta name="robots" content="follow,index,nocache"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="description" content="Royal :: Gana con Royal"/>
+    <meta name="author" content="Misiva Corp"/>
+
+    <meta property="og:title" content="Gana con Royal">
+    <meta property="og:type" content="article">
+    <!--    <meta property="og:url" content="http://www.ganaconroyal.com">-->
+    <meta property="og:url" content="http://royal.misiva.com.ec/index">
+    <meta property="og:image" content="http://royal.misiva.com.ec/imagenes/royal_ruleta/facebook.jpg">
+    <meta property="og:site_name" content="GanaConRoyal">
+    <!--<meta property="fb:admins" content="100001543332907">-->
+    <meta property="og:description"
+          content="Yo ya estoy participando para ganar un viaje a Fox Studios en Los Ángeles  con Royal y por muchos premios más. Y tú… ¿qué esperas? Compra tu Gelatina Royal, ingresa el código de tu empaque y gana..">
+
+
+    <link href="http://localhost:10088/royal/css/royal_ruleta/bootstrap.min.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+
+    <link rel="icon" type="image/png" sizes="96x96" href="imagenes/royal_ruleta/favicon/favicon-96x96.png"/>
+    <link rel="icon" type="image/png" sizes="32x32" href="imagenes/royal_ruleta/favicon/favicon-32x32.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="imagenes/royal_ruleta/favicon/favicon-16x16.png"/>
+    <link rel="apple-touch-icon" sizes="57x57" href="imagenes/royal_ruleta/favicon/apple-touch-114x114.png"/>
+<style>
+    .inicio  {
+        position: inherit;
+    }
+
+    .inicio {
+        width: 100%;
+        height: 100%;
+        background-size: cover;
+        position: absolute;
+        padding-top: 0;
+    }
+
+    .inicio {
+        background-image: url("imagenes/royal_ruleta/proximamente.jpg");
+        background-repeat: no-repeat;
+        background-position: center center;
+    }
+</style>
+</head>
+<body>
+<div class="inicio" >
+</div>
+<script>
+    //Google Analitics
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-2423727-40', 'auto');
+    ga('send', 'pageview');
+
+</script>
+</body>
+</html>
