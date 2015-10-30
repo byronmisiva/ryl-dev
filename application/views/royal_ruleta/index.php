5f68731b55454dca6b3160faf939a3d4727b898a
Byron <byronherrera@hotmail.com>
2015-10-30 10:03:51 -0500

    aa

--- application/views/royal_ruleta/index.php
+++ application/views/royal_ruleta/index.php
@@ -20,7 +20,18 @@
     <script src="<?php echo base_url() ?>js/royal_ruleta/bootstrap.min.js"></script>
     <script src="<?php echo base_url() ?>js/royal_ruleta/ligthbox/ekko-lightbox.min.js"></script>
 
+
+
     <script language="JavaScript" src="<?php echo base_url() ?>js/royal_ruleta/app.js"></script>
+
+    <meta property="og:title" content="Gana con Royal">
+    <meta property="og:type" content="article">
+    <meta property="og:url" content="http://www.ganaconroyal.com">
+    <meta property="og:image" content="http://norfipc.com/img/logos/compartir-pagina-redes-sociales.jpeg">
+    <meta property="og:site_name" content="GanaConRoyal">
+<meta property="fb:admins" content="100001543332907">
+    <meta property="og:description" content="Gana con Royal premios.">
+
 
     <link href="<?php echo base_url() ?>fonts/Roboto-Bold/styles.css" rel="stylesheet" type="text/css"/>
     <link href="<?php echo base_url() ?>fonts/Roboto-Light/styles.css" rel="stylesheet" type="text/css"/>
@@ -71,73 +82,73 @@
             </div>
         </div>
 
-<!--        <div class="container center-block">-->
+        <!--        <div class="container center-block">-->
         <div class=" ">
             <div class="espacio74 ">
                 <div class="col-md-12 col-sm-12">
 
 
-                        <div class="col-sm-offset-4 col-md-offset-4 col-xs-offset-4  col-md-8 col-sm-8 col-xs-8">
-                            <div class="titulares titulo text-center"><p>REGISTRO</p></div>
-                        </div>
-                        <div class="col-sm-offset-3 col-md-offset-3 col-xs-offset-3  col-md-8 col-sm-8 col-xs-8">
-                            <form id="registroform"
-                                  action=""
-                                  method="post"
-                                  enctype="multipart/form-data">
-                                <ul class="login_wid">
-                                    <li><input class="box-text" type="text" id="cedula" name="cedula" maxlength="10"
+                    <div class="col-sm-offset-4 col-md-offset-4 col-xs-offset-4  col-md-8 col-sm-8 col-xs-8">
+                        <div class="titulares titulo text-center"><p>REGISTRO</p></div>
+                    </div>
+                    <div class="col-sm-offset-3 col-md-offset-3 col-xs-offset-3  col-md-8 col-sm-8 col-xs-8">
+                        <form id="registroform"
+                              action=""
+                              method="post"
+                              enctype="multipart/form-data">
+                            <ul class="login_wid">
+                                <li><input class="box-text" type="text" id="cedula" name="cedula" maxlength="10"
+                                           required="required"
+                                           placeholder="Cédula:"></li>
+                                <div id="complete_register" class="hidden">
+                                    <li><input class="box-text" type="text" id="nombre" name="nombre" maxlength="20"
                                                required="required"
-                                               placeholder="Cédula:"></li>
-                                    <div id="complete_register" class="hidden">
-                                        <li><input class="box-text" type="text" id="nombre" name="nombre" maxlength="20"
-                                                   required="required"
-                                                   placeholder="Nombre:">
-                                        </li>
-                                        <li><input class="box-text" type="text" id="apellido" name="apellido"
-                                                   maxlength="20"
-                                                   required="required"
-                                                   placeholder="Apellido:">
-                                        </li>
-                                        <li><input class="box-text" type="email" id="mail" name="mail" maxlength="100"
-                                                   required="required"
-                                                   placeholder="Email:">
-                                        </li>
-                                        <li><input class="box-text" type="text" id="telefono" name="telefono"
-                                                   maxlength="10"
-                                                   required="required"
-                                                   placeholder="Teléfono:"></li>
-                                        <li><input class="box-text" type="text" id="ciudad" name="ciudad" maxlength="20"
-                                                   required="required"
-                                                   placeholder="Ciudad:">
-                                        </li>
+                                               placeholder="Nombre:">
+                                    </li>
+                                    <li><input class="box-text" type="text" id="apellido" name="apellido"
+                                               maxlength="20"
+                                               required="required"
+                                               placeholder="Apellido:">
+                                    </li>
+                                    <li><input class="box-text" type="email" id="mail" name="mail" maxlength="100"
+                                               required="required"
+                                               placeholder="Email:">
+                                    </li>
+                                    <li><input class="box-text" type="text" id="telefono" name="telefono"
+                                               maxlength="10"
+                                               required="required"
+                                               placeholder="Teléfono:"></li>
+                                    <li><input class="box-text" type="text" id="ciudad" name="ciudad" maxlength="20"
+                                               required="required"
+                                               placeholder="Ciudad:">
+                                    </li>
 
-                                        <li><input class="box-text" type="text" id="box-codigo1" name="box-codigo1"
-                                                   maxlength="20"
-                                                   required="required"
-                                                   placeholder="Código de tu producto:">
-                                        </li>
+                                    <li><input class="box-text" type="text" id="box-codigo1" name="box-codigo1"
+                                               maxlength="20"
+                                               required="required"
+                                               placeholder="Código de tu producto:">
+                                    </li>
 
-                                        <li class="text-center">
-                                            <div class="portabotones center-block ">
-                                                <input type="submit" value="Continuar" name="login"
-                                                       class="btn-ingreso botontexto"
-                                                       style="">
-                                            </div>
-                                        </li>
-                                        <li class="text-center">
-                                            <div id="mensaje-envio"></div>
-                                        </li>
-                                        <li class="text-center">
-                                            <a href="archivos/REGLAMENTO-DE-TERMINOS-Y-CONDICIONES-PARA-EL-CONCURSO-Karaoke.pdf"
-                                               target="_blank">Términos y
-                                                condiciones</a>
-                                        </li>
-                                    </div>
-                                </ul>
-                                <input type="text" id="fbid" name="fbid" class="hidden">
-                            </form>
-                        </div>
+                                    <li class="text-center">
+                                        <div class="portabotones center-block ">
+                                            <input type="submit" value="Continuar" name="login"
+                                                   class="btn-ingreso botontexto"
+                                                   style="">
+                                        </div>
+                                    </li>
+                                    <li class="text-center">
+                                        <div id="mensaje-envio"></div>
+                                    </li>
+                                    <li class="text-center">
+                                        <a href="archivos/REGLAMENTO-DE-TERMINOS-Y-CONDICIONES-PARA-EL-CONCURSO-Karaoke.pdf"
+                                           target="_blank">Términos y
+                                            condiciones</a>
+                                    </li>
+                                </div>
+                            </ul>
+                            <input type="text" id="fbid" name="fbid" class="hidden">
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
@@ -152,6 +163,9 @@
         <div class="pestaneo2"></div>
         <div class="pestaneo3"></div>
         <div class="botones">
+            <a href="javascript:var dir=window.document.URL;var tit=window.document.title;var tit2=encodeURIComponent(tit);var dir2= encodeURIComponent(dir);window.location.href=('http://www.facebook.com/share.php?u='+dir2+'&amp;t='+tit2+'');">Compartir
+                en Facebook</a>
+
             <div class="btn-instrucciones js--triggerInstrucciones"></div>
             <div class="btn-audioOn js--triggerAudio"></div>
             <div class="btn-compartir js--triggerCompartir"></div>
@@ -220,6 +234,12 @@
         </div>
     </div>
 </div>
+<script type="text/javascript">
+    var dir = window.document.URL;
+    var dir2 = encodeURIComponent(dir);
+    var tit = window.document.title;
+    var tit2 = encodeURIComponent(tit);
+</script>
 
 <script type="text/javascript" charset="utf-8">
     var idParticipante = 0;
