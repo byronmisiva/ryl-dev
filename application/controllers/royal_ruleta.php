183d05d0c4e963b52b0428ef69177a2993d9c731
Byron <byronherrera@hotmail.com>
2015-11-04 17:25:12 -0500

    aa

--- application/controllers/royal_ruleta.php
+++ application/controllers/royal_ruleta.php
@@ -160,10 +160,10 @@ class Royal_ruleta extends CI_Controller
     {
         $this->load->library('email');
 
-        $this->email->from('your@example.com', 'Your Name');
-        $this->email->to('someone@example.com');
-        $this->email->cc('another@another-example.com');
-        $this->email->bcc('them@their-example.com');
+        $this->email->from('info@ganaconroyal.com', 'Your Name');
+        $this->email->to('bherrera@misiva.com.ec');
+        //$this->email->cc('another@another-example.com');
+        //$this->email->bcc('them@their-example.com');
 
         $this->email->subject('Email Test');
         $this->email->message('Testing the email class.');
