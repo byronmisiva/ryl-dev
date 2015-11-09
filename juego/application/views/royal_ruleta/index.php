04ece7dbeadd710e618993536d846f8e813aaa03
Byron <byronherrera@hotmail.com>
2015-11-09 18:03:40 -0500

    aa

--- juego/application/views/royal_ruleta/index.php
+++ juego/application/views/royal_ruleta/index.php
@@ -307,7 +307,7 @@
         <img src="imagenes/royal_ruleta/snoopy-juego/legales.png" class="img-responsive center-img">
     </div>
 </div>
-<div id="instrucciones" class="hidden seccion hidden-xs">
+<div id="instrucciones" class="seccion hidden-xs">
     <div class="container vertical-center">
         <div class="col-md-12 col-sm-12 col-xs-12">
             <img src="imagenes/royal_ruleta/snoopy-juego/instrucciones.png" class="img-responsive center-img">
