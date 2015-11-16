828d367ce0183e44e420693a3207e898a8c1e7f5
Byron <byronherrera@hotmail.com>
2015-11-16 16:38:42 -0500

    aa

--- application/views/royal_ruleta/index.php
+++ application/views/royal_ruleta/index.php
@@ -150,7 +150,7 @@
                                         </div>
                                         <div class="col-md-9 col-sm-9 col-xs-9 margen-0">
                                             <input class="box-text" type="text" id="telefono" name="telefono"
-                                                   maxlength="10"
+                                                   maxlength="12"
                                                    required="required">
                                         </div>
                                     </li>
